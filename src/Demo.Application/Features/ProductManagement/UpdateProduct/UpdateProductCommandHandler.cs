38cd97b70b541c90fc9ae3eaecc80134f3f7ed39
Arsanyos <arsanyos1600@gmail.com>
2026-07-28 17:13:47 +0300

    fix: updatecommandhandler extends Irequesthandler

--- src/Demo.Application/Features/ProductManagement/UpdateProduct/UpdateProductCommandHandler.cs
+++ src/Demo.Application/Features/ProductManagement/UpdateProduct/UpdateProductCommandHandler.cs
@@ -9,7 +9,7 @@ namespace Demo.Application.Features.ProductManagement.UpdateProduct;
 public sealed class UpdateProductCommandHandler(
     IProductRepository productRepository,
     IUnitOfWork unitOfWork) 
-    : ICommandHandler<UpdateProductCommand, UpdateProductDto>
+    : ICommandHandler<UpdateProductCommand, UpdateProductDto>, IRequestHandler<UpdateProductCommand, UpdateProductDto>
 {
     public async Task<UpdateProductDto> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
     {
